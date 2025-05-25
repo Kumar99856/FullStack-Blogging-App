90905684c08e8589633bd294ab644df245bb1182
Kumar99856 <satyakumar99856@gmail.com>
2025-05-25 17:57:02 +0530

    Update main.tf

--- EKS_Terraform/main.tf
+++ EKS_Terraform/main.tf
@@ -2,55 +2,55 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "devopsshack_vpc" {
+resource "aws_vpc" "Multi-Tier-Devops_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "devopsshack-vpc"
+    Name = "Multi-Tier-Devops-vpc"
   }
 }
 
-resource "aws_subnet" "devopsshack_subnet" {
+resource "aws_subnet" "Multi-Tier-Devops_subnet" {
   count = 2
-  vpc_id                  = aws_vpc.devopsshack_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.devopsshack_vpc.cidr_block, 8, count.index)
-  availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
+  vpc_id                  = aws_vpc.Multi-Tier-Devops_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.Multi-Tier-Devops_vpc.cidr_block, 8, count.index)
+  availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "devopsshack-subnet-${count.index}"
+    Name = "Multi-Tier-Devops-subnet-${count.index}"
   }
 }
 
-resource "aws_internet_gateway" "devopsshack_igw" {
-  vpc_id = aws_vpc.devopsshack_vpc.id
+resource "aws_internet_gateway" "Multi-Tier-Devops_igw" {
+  vpc_id = aws_vpc.Multi-Tier-Devops_vpc.id
 
   tags = {
-    Name = "devopsshack-igw"
+    Name = "Multi-Tier-Devops-igw"
   }
 }
 
-resource "aws_route_table" "devopsshack_route_table" {
-  vpc_id = aws_vpc.devopsshack_vpc.id
+resource "aws_route_table" "Multi-Tier-Devops_route_table" {
+  vpc_id = aws_vpc.Multi-Tier-Devops_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.devopsshack_igw.id
+    gateway_id = aws_internet_gateway.Multi-Tier-Devops_igw.id
   }
 
   tags = {
-    Name = "devopsshack-route-table"
+    Name = "Multi-Tier-Devops-route-table"
   }
 }
 
 resource "aws_route_table_association" "a" {
   count          = 2
-  subnet_id      = aws_subnet.devopsshack_subnet[count.index].id
-  route_table_id = aws_route_table.devopsshack_route_table.id
+  subnet_id      = aws_subnet.Multi-Tier-Devops_subnet[count.index].id
+  route_table_id = aws_route_table.Multi-Tier-Devops_route_table.id
 }
 
-resource "aws_security_group" "devopsshack_cluster_sg" {
-  vpc_id = aws_vpc.devopsshack_vpc.id
+resource "aws_security_group" "Multi-Tier-Devops_cluster_sg" {
+  vpc_id = aws_vpc.Multi-Tier-Devops_vpc.id
 
   egress {
     from_port   = 0
@@ -60,12 +60,12 @@ resource "aws_security_group" "devopsshack_cluster_sg" {
   }
 
   tags = {
-    Name = "devopsshack-cluster-sg"
+    Name = "Multi-Tier-Devops-cluster-sg"
   }
 }
 
-resource "aws_security_group" "devopsshack_node_sg" {
-  vpc_id = aws_vpc.devopsshack_vpc.id
+resource "aws_security_group" "Multi-Tier-Devops_node_sg" {
+  vpc_id = aws_vpc.Multi-Tier-Devops_vpc.id
 
   ingress {
     from_port   = 0
@@ -82,17 +82,17 @@ resource "aws_security_group" "devopsshack_node_sg" {
   }
 
   tags = {
-    Name = "devopsshack-node-sg"
+    Name = "Multi-Tier-Devops-node-sg"
   }
 }
 
-resource "aws_eks_cluster" "devopsshack" {
-  name     = "devopsshack-cluster"
-  role_arn = aws_iam_role.devopsshack_cluster_role.arn
+resource "aws_eks_cluster" "Multi-Tier-Devops" {
+  name     = "Multi-Tier-Devops-cluster"
+  role_arn = aws_iam_role.Multi-Tier-Devops_cluster_role.arn
 
   vpc_config {
-    subnet_ids         = aws_subnet.devopsshack_subnet[*].id
-    security_group_ids = [aws_security_group.devopsshack_cluster_sg.id]
+    subnet_ids         = aws_subnet.Multi-Tier-Devopssubnet[*].id
+    security_group_ids = [aws_security_group.Multi-Tier-Devops_cluster_sg.id]
   }
 }
 
@@ -116,8 +116,8 @@ resource "aws_eks_node_group" "Multi-Tier-Devops" {
   }
 }
 
-resource "aws_iam_role" "devopsshack_cluster_role" {
-  name = "devopsshack-cluster-role"
+resource "aws_iam_role" "Multi-Tier-Devops_cluster_role" {
+  name = "Multi-Tier-Devops-cluster-role"
 
   assume_role_policy = <<EOF
 {
@@ -135,13 +135,13 @@ resource "aws_iam_role" "devopsshack_cluster_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_cluster_role_policy" {
-  role       = aws_iam_role.devopsshack_cluster_role.name
+resource "aws_iam_role_policy_attachment" "Multi-Tier-Devops_cluster_role_policy" {
+  role       = aws_iam_role.Multi-Tier-Devops_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role" "devopsshack_node_group_role" {
-  name = "devopsshack-node-group-role"
+resource "aws_iam_role" "Multi-Tier-Devops_node_group_role" {
+  name = "Multi-Tier-Devops-node-group-role"
 
   assume_role_policy = <<EOF
 {
@@ -159,17 +159,17 @@ resource "aws_iam_role" "devopsshack_node_group_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_node_group_role_policy" {
-  role       = aws_iam_role.devopsshack_node_group_role.name
+resource "aws_iam_role_policy_attachment" "Multi-Tier-Devops_node_group_role_policy" {
+  role       = aws_iam_role.Multi-Tier-Devops_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_node_group_cni_policy" {
-  role       = aws_iam_role.devopsshack_node_group_role.name
+resource "aws_iam_role_policy_attachment" "Multi-Tier-Devops_node_group_cni_policy" {
+  role       = aws_iam_role.Multi-Tier-Devops_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_node_group_registry_policy" {
-  role       = aws_iam_role.devopsshack_node_group_role.name
+resource "aws_iam_role_policy_attachment" "Multi-Tier-Devops_node_group_registry_policy" {
+  role       = aws_iam_role.Multi-Tier-Devops_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
