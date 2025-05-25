ba418d7a8c876f2497a71f61796a6f1852e2b092
Kumar99856 <satyakumar99856@gmail.com>
2025-05-25 17:46:15 +0530

    Update main.tf

--- EKS_Terraform/main.tf
+++ EKS_Terraform/main.tf
@@ -100,7 +100,7 @@ resource "aws_eks_node_group" "Multi-Tier-Devops" {
   cluster_name    = aws_eks_cluster.Multi-Tier-Devops
   node_group_name = "Multi-Tier-Devops-node-group"
   node_role_arn   = aws_iam_role.Multi-Tier-Devops_node_group_role.arn
-  subnet_ids      = aws_subnet.devopsshac_subnet[*].id
+  subnet_ids      = aws_subnet.Multi-Tier-Devops_subnet[*].id
 
   scaling_config {
     desired_size = 3
